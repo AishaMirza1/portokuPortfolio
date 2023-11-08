7994f4331e76adc1e28809c7db97a311a1f88a16
Home <aishamirza0002@gmail.com>
2023-11-08 22:21:06 +0530

    portfoloes

--- src/components/sections/featuredWorkSection/FeaturedWorkSection.jsx
+++ src/components/sections/featuredWorkSection/FeaturedWorkSection.jsx
@@ -1,4 +1,5 @@
 import styles from './featuredWorkSection.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
 export default function FeaturedWorkSection() {
   return (
     <section className={styles.featuredWorkSection} id="portfolios">
@@ -7,7 +8,36 @@ export default function FeaturedWorkSection() {
         <p>I&apos;ve Contributed Efforts to Achieve Bigger Results</p>
       </div>
       <div>
-        <p>projects scrollbar</p>
+        <div>
+          <AnimatePresence>
+            <motion.img
+              whileHover={{
+                scale: 1.1,
+                translateX: 22,
+                transition: { duration: 0.3 },
+              }}
+              exit={{ zIndex: 0 }}
+              src="https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg"
+            ></motion.img>
+
+            <motion.img
+              whileHover={{
+                scale: 1.1,
+                translateX: 22,
+                transition: { duration: 0.3 },
+              }}
+              src="https://img.freepik.com/premium-vector/project-concept-vector-business-horizontal-illustration-banner-white-background_104589-19.jpg"
+            ></motion.img>
+            <motion.img
+              whileHover={{
+                scale: 1.01,
+                translateX: 22,
+                transition: { duration: 0.3 },
+              }}
+              src="https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg"
+            ></motion.img>
+          </AnimatePresence>
+        </div>
       </div>
       <div className={styles.featuredWorkSectionStats}>
         <ul>
